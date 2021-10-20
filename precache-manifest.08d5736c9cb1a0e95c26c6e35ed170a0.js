6cfa58e468cc27dbf2ac6aeea4c16fe272def56f
unknown <alextq3036@gmail.com>
2021-10-20 15:56:43 -0400

    Updates

---
+++ precache-manifest.08d5736c9cb1a0e95c26c6e35ed170a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496e94983e58f3091a1ad983408d040b",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "2ab99fc24a0f50da4278",
+    "url": "/pomodoro-clock/static/css/main.884214d6.chunk.css"
+  },
+  {
+    "revision": "1e1f30b7ca72379a6e10",
+    "url": "/pomodoro-clock/static/js/2.9e345789.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pomodoro-clock/static/js/2.9e345789.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ab99fc24a0f50da4278",
+    "url": "/pomodoro-clock/static/js/main.87554be4.chunk.js"
+  },
+  {
+    "revision": "3c30f5ec6bc54f730a4a",
+    "url": "/pomodoro-clock/static/js/runtime-main.8b2866e8.js"
+  }
+]);
